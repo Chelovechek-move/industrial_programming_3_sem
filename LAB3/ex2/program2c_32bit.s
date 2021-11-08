83b877fbc07ef45137035c1678ebf015329378c3
Chelovechek-move <81492323+Chelovechek-move@users.noreply.github.com>
2021-11-08 20:18:16 +0300

    Add files via upload

---
+++ LAB3/ex2/program2c_32bit.s
@@ -0,0 +1,68 @@
+	.file	"program2c.c"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	endbr32
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	andl	$-16, %esp
+	subl	$32, %esp
+	call	__x86.get_pc_thunk.ax
+	addl	$_GLOBAL_OFFSET_TABLE_, %eax
+	movl	%gs:20, %eax
+	movl	%eax, 28(%esp)
+	xorl	%eax, %eax
+	movl	$1, 8(%esp)
+	movl	$2, 12(%esp)
+	movl	$3, 16(%esp)
+	movl	$4, 20(%esp)
+	movl	$5, 24(%esp)
+	movl	$0, %eax
+	movl	28(%esp), %edx
+	xorl	%gs:20, %edx
+	je	.L3
+	call	__stack_chk_fail_local
+.L3:
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.section	.text.__x86.get_pc_thunk.ax,"axG",@progbits,__x86.get_pc_thunk.ax,comdat
+	.globl	__x86.get_pc_thunk.ax
+	.hidden	__x86.get_pc_thunk.ax
+	.type	__x86.get_pc_thunk.ax, @function
+__x86.get_pc_thunk.ax:
+.LFB1:
+	.cfi_startproc
+	movl	(%esp), %eax
+	ret
+	.cfi_endproc
+.LFE1:
+	.hidden	__stack_chk_fail_local
+	.ident	"GCC: (Ubuntu 9.3.0-17ubuntu1~20.04) 9.3.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 4
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 4
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 4
+4:
