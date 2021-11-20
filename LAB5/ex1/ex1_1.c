579770c2e0a06cc7939bd6d5c27ddfc2d0f97a17
Chelovechek-move <81492323+Chelovechek-move@users.noreply.github.com>
2021-11-20 21:01:55 +0300

    Add files via upload

---
+++ LAB5/ex1/ex1_1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int a = 101;
+int k = 0;
+
+int main()
+{
+	printf("%d\n", a);
+
+	asm
+	(
+		"movl	a(%rip), %ebx\n"	// %ebx = a
+	);
+
+	for(int i = 0; i < 32; i++)
+	{
+		asm
+		(
+			"rol	$1, %ebx\n"		// сyclic shift left by 1
+			"setc	k(%rip)\n"		// set byte value if CF = 1
+		);
+
+		printf("%d", k);
+	}
+}
